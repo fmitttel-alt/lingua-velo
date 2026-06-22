dc8afe6eb1a09aedcd74983ae84c2199fac4b924
fmitttel_git_hub <fmitttel@gmail.com>
2026-06-22 21:24:40 +0200

    fix: remove WakeWordDetector from MainTabView to fix crash on launch

--- LinguaVelo/Views/Components/MainTabView.swift
+++ LinguaVelo/Views/Components/MainTabView.swift
@@ -2,7 +2,6 @@ import SwiftUI
 
 struct MainTabView: View {
     @State private var selectedTab: Tab = .dashboard
-    @StateObject private var wakeWord = WakeWordDetector()
 
     enum Tab: String, CaseIterable {
         case dashboard = "Oggi"
@@ -46,8 +45,5 @@ struct MainTabView: View {
         }
         .tint(AppTheme.Colors.salmon)
         .background(AppTheme.Colors.background)
-        .environmentObject(wakeWord)
-        .onAppear { wakeWord.start() }
-        .onDisappear { wakeWord.stop() }
     }
 }
